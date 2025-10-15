5706131ca5938a9cb70e16560e9dbef77b144022
Guilherme <guilherme.falcao.s@live.com>
2025-10-14 22:03:46 -0300

    retornando as tarefas

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from flask import Flask
 
+from tarefa import buscar_tarefas
+
 app = Flask(__name__)
 
 @app.route('/api', methods=['GET'])
@@ -7,6 +9,11 @@ def index():
     return{
         'message' : 'Api rodando'
     }
+
+@app.route('/api/tarefas', methods=['GET'])
+def get_tarefas():
+    tarefas= buscar_tarefas()
+    return tarefas
 
 if __name__ =='__main__':
     app.run(debug=True)
---
+++ tarefa.py
@@ -0,0 +1,20 @@
+from flask import jsonify
+
+def buscar_tarefas():
+    tarefas = [
+        {
+                'id': 1,
+                'nome': 'aprender digitação',
+                'descicao': 'Vamos aumentar o zoom para não errar a digitação' ,
+                'status': 'Pendente'
+        },
+
+        {
+            'id': 2,
+            'nome': 'Aprender pyhthon',
+            'descricao': 'Aprender python para programar JSON',
+            'ststus': 'Pendente'
+        }
+    ]
+
+    return jsonify(tarefas)
